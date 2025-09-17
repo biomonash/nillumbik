eeae809071e5b56fbb6642bf948402d8a0b02892
Justin Lin <wancat@wancat.cc>
2025-09-17 16:32:58 +1000

    refactor: move gin server code to server package

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -5,17 +5,14 @@ import (
 	"log"
 	"os"
 
-	"github.com/gin-gonic/gin"
 	"github.com/jackc/pgx/v5"
 	"github.com/joho/godotenv"
-	swaggerFiles "github.com/swaggo/files"     // swagger embed files
-	ginSwagger "github.com/swaggo/gin-swagger" // gin-swagger middleware
 
+	// swagger embed files
+	// gin-swagger middleware
 	_ "github.com/biomonash/nillumbik/docs"
 	"github.com/biomonash/nillumbik/internal/db"
-	"github.com/biomonash/nillumbik/internal/observation"
-	"github.com/biomonash/nillumbik/internal/site"
-	"github.com/biomonash/nillumbik/internal/species"
+	"github.com/biomonash/nillumbik/internal/server"
 )
 
 func init() {
@@ -25,25 +22,6 @@ func init() {
 	}
 }
 
-//	@title			Nillubim Shire API
-//	@version		1.0
-//	@description	This is the backend API for Nillumbik Shire project.
-//	@termsOfService	http://swagger.io/terms/
-
-//	@contact.name	API Support
-//	@contact.url	http://www.swagger.io/support
-//	@contact.email	support@swagger.io
-
-//	@license.name	Apache 2.0
-//	@license.url	http://www.apache.org/licenses/LICENSE-2.0.html
-
-//	@host		localhost:8000
-//	@BasePath	/api/
-
-//	@securityDefinitions.basic	BasicAuth
-
-// @externalDocs.description	OpenAPI
-// @externalDocs.url			https://swagger.io/resources/open-api/
 func run() error {
 	ctx := context.Background()
 
@@ -55,20 +33,10 @@ func run() error {
 	defer conn.Close(ctx)
 
 	querier := db.New(conn)
-	r := gin.Default()
 
-	api := r.Group("/api")
+	s := server.New(querier)
 
-	site.Register(api, site.NewController(querier))
-
-	species.Register(api, species.NewController(querier))
-
-	observation.Register(api, observation.NewController(querier))
-
-	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
-
-	r.Run(":8000")
-	return nil
+	return s.Run(":8000")
 }
 
 func main() {
