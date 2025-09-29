54a68c8f1988ade92536f25882cdfb3cdf997b52
Justin Lin <wancat@wancat.cc>
2025-09-29 17:09:21 +1000

    feat: change backend API field to camelCase

--- backend/go.mod
+++ backend/go.mod
@@ -3,6 +3,7 @@ module github.com/biomonash/nillumbik
 go 1.24.6
 
 require (
+	github.com/gin-contrib/cors v1.7.6
 	github.com/gin-gonic/gin v1.10.1
 	github.com/jackc/pgx/v5 v5.7.6
 	github.com/joho/godotenv v1.5.1
@@ -18,7 +19,6 @@ require (
 	github.com/bytedance/sonic/loader v0.3.0 // indirect
 	github.com/cloudwego/base64x v0.1.6 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.10 // indirect
-	github.com/gin-contrib/cors v1.7.6 // indirect
 	github.com/gin-contrib/sse v1.1.0 // indirect
 	github.com/go-openapi/jsonpointer v0.22.0 // indirect
 	github.com/go-openapi/jsonreference v0.21.1 // indirect
--- backend/internal/stats/model.go
+++ backend/internal/stats/model.go
@@ -14,14 +14,14 @@ type TimePeriodRequest struct {
 type ObservationStatsInput struct {
 	TimePeriodRequest
 	Block      *int32   `form:"block"`
-	SiteCode   *string  `form:"site_code"`
+	SiteCode   *string  `form:"siteCode"`
 	Taxa       *db.Taxa `form:"taxa"`
-	CommonName *string  `form:"common_name"`
+	CommonName *string  `form:"commonName"`
 }
 
 type ObservationStats struct {
-	ObservationCount int64 `json:"observation_count"`
-	SpeciesCount     int64 `json:"species_count"`
+	ObservationCount int64 `json:"observationCount"`
+	SpeciesCount     int64 `json:"speciesCount"`
 }
 
 type TimeSeriesPoint struct {
