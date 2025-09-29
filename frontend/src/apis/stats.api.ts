f5bf7636cc3830a9c9e296f6012fb9aa83bb1120
Justin Lin <wancat@wancat.cc>
2025-09-29 17:57:12 +1000

    feat: add observation timeseries api

--- frontend/src/apis/stats.api.ts
+++ frontend/src/apis/stats.api.ts
@@ -16,11 +16,33 @@ export type ObservationOverviewResponse = {
   countByTaxa: Record<string, string>;
 };
 
+export type ObservationTimeseriesResponse = {
+  series: Record<string, TimeseriesPoint[]>;
+};
+
+export type TimeseriesPoint = {
+  timestamp: string;
+  observationCount: number;
+  speciesCount: number;
+};
+
 export async function getObservationsOverview(
   req: Partial<ObservationStatsRequest>,
 ): Promise<ObservationOverviewResponse> {
   const response = await fetcher.get<ObservationOverviewResponse>(
     "/stats/observations",
+    {
+      params: req,
+    },
+  );
+  return response.data;
+}
+
+export async function getObservationsTimeseries(
+  req: Partial<ObservationStatsRequest>,
+): Promise<ObservationTimeseriesResponse> {
+  const response = await fetcher.get<ObservationTimeseriesResponse>(
+    "/stats/observations/timeseries",
     {
       params: req,
     },
--- frontend/src/features/map/components/ApiDisplay.tsx
+++ frontend/src/features/map/components/ApiDisplay.tsx
@@ -6,19 +6,26 @@ import { useEffect, useState } from "react";
 import type { JSX } from "react/jsx-dev-runtime";
 import {
   getObservationsOverview,
+  getObservationsTimeseries,
+  type ObservationStatsRequest,
   type ObservationOverviewResponse,
+  type ObservationTimeseriesResponse,
 } from "../../../apis/stats.api";
 
 const ApiDisplay: React.FC = (): JSX.Element => {
+  const [filter, setFilter] = useState<Partial<ObservationStatsRequest>>({});
   const [overview, setOverview] = useState<ObservationOverviewResponse | null>(
     null,
   );
+  const [timeseries, setTimeseries] =
+    useState<ObservationTimeseriesResponse | null>(null);
 
   useEffect(() => {
-    getObservationsOverview({}).then(setOverview);
-  }, []);
+    getObservationsOverview(filter).then(setOverview);
+    getObservationsTimeseries(filter).then(setTimeseries);
+  }, [filter]);
 
-  if (overview === null) {
+  if (overview === null || timeseries === null) {
     return <p>Loading</p>;
   }
 
@@ -27,21 +34,40 @@ const ApiDisplay: React.FC = (): JSX.Element => {
       {key}: {overview.countByTaxa[key]}
     </li>
   ));
+
+  const timeseriesList = Object.keys(timeseries.series).map((key) => (
+    <div key={key}>
+      <p>{key}</p>
+      <ul>
+        {timeseries.series[key].map(
+          ({ timestamp, observationCount, speciesCount }) => (
+            <li key={timestamp}>
+              {timestamp}: Observations: {observationCount}. Species:{" "}
+              {speciesCount}
+            </li>
+          ),
+        )}
+      </ul>
+    </div>
+  ));
+
   return (
     <div>
       <p>This is API Debug section, should be removed later</p>
-      <p>
-        <div>
-          <p>Observation count: {overview.observationCount}</p>
-          <p>Unique species count: {overview.speciesCount}</p>
-          <p>Native species count: {overview.nativeSpeciesCount}</p>
-          <p>
-            Non-native species count:{" "}
-            {overview.speciesCount - overview.nativeSpeciesCount}
-          </p>
-          <ul>{taxaList}</ul>
-        </div>
-      </p>
+      <div>
+        <p>Observation count: {overview.observationCount}</p>
+        <p>Unique species count: {overview.speciesCount}</p>
+        <p>Native species count: {overview.nativeSpeciesCount}</p>
+        <p>
+          Non-native species count:{" "}
+          {overview.speciesCount - overview.nativeSpeciesCount}
+        </p>
+        <ul>{taxaList}</ul>
+      </div>
+      <div>
+        <h2>Timeseries</h2>
+        {timeseriesList}
+      </div>
     </div>
   );
 };
