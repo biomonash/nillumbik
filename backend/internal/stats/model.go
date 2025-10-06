5fac5130f74a9bdf26b45209d507c20bdec66d3d
Justin Lin <wancat@wancat.cc>
2025-10-07 10:13:12 +1100

    feat: integrate dashboard stats to frontend

--- backend/internal/stats/model.go
+++ backend/internal/stats/model.go
@@ -7,8 +7,8 @@ import (
 )
 
 type TimePeriodRequest struct {
-	From *time.Time `form:"from" time_format:"2006-01-02"`
-	To   *time.Time `form:"to"   time_format:"2006-01-02"`
+	From *time.Time `form:"from"`
+	To   *time.Time `form:"to"`
 }
 
 type ObservationStatsInput struct {
