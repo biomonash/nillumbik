488a981b5772e50b4ccb88653db67600e7631178
Justin Lin <wancat@wancat.cc>
2025-11-06 11:44:35 +1100

    feat: import map

--- frontend/src/features/map/index.tsx
+++ frontend/src/features/map/index.tsx
@@ -1,11 +1,23 @@
 import React, { type JSX } from "react";
-import ApiDisplay from "./components/ApiDisplay";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+// import ApiDisplay from "./components/ApiDisplay";
 
 const Map: React.FC = (): JSX.Element => {
   return (
-    <section>
-      Map Page
-      <ApiDisplay />
+    <section style={{ width: "100%", height: "100vh" }}>
+      <MapContainer
+        center={[51.505, -0.09] as const}
+        zoom={13}
+        scrollWheelZoom={false}
+        style={{ width: "100%", height: "100%" }}
+      >
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        <Marker position={[51.505, -0.09]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
     </section>
   );
 };
