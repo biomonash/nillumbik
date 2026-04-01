d59d23a7d46df1059f9879ed40897102af5b5bb5
Minseyy <yanyennhui2005@gmail.com>
2026-04-01 21:20:53 +1100

    Partially completed side bars for map page

--- frontend/src/features/map/index.tsx
+++ frontend/src/features/map/index.tsx
@@ -1,9 +1,12 @@
 import MapView from "./components/MapView";
+import MapStats from "../../components/ui/MapStats";
+import InfoStats from "../../components/ui/InfoStats";
 
 export default function MapPage() {
   return (
     <div style={{ position: "relative" }}>
-      
+      <InfoStats/>
+      <MapStats/>
       {/* Background Map */}
       <MapView />
 
@@ -11,7 +14,6 @@ export default function MapPage() {
       <div style={{ position: "relative", zIndex: 10 }}>
         <h1>Map Page</h1>
       </div>
-
     </div>
   );
 }
