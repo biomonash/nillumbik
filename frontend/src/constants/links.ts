33c0519fa79d927ccc7296bf5a2bbcc978c493b0
Justin Lin <wancat@wancat.cc>
2026-07-27 16:46:24 +1000

    fix: format

--- frontend/src/constants/links.ts
+++ frontend/src/constants/links.ts
@@ -1,7 +1,7 @@
 export default {
-  WEBSITE: "https://biomonash.github.io/",
-  GITHUB: "https://github.com/biomonash/nillumbik",
-  LINKEDIN: "https://www.linkedin.com/company/biologicalobservationmonash",
-  INSTAGRAM: "https://www.instagram.com/biomonash/",
-  EMAIL: "mailto:biom@monash.edu",
+  WEBSITE: 'https://biomonash.github.io/',
+  GITHUB: 'https://github.com/biomonash/nillumbik',
+  LINKEDIN: 'https://www.linkedin.com/company/biologicalobservationmonash',
+  INSTAGRAM: 'https://www.instagram.com/biomonash/',
+  EMAIL: 'mailto:biom@monash.edu',
 }
