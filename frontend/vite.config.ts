e45d9eca22cbf102436aac19a7e717c0741dd045
Justin Lin <wancat@wancat.cc>
2026-07-27 16:24:18 +1000

    feat: add github page workflow

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: "/forestportal",
+  base: loadEnv(mode, '.').VITE_BASE_PATH || '/forestportal',
   server: {
     port: 3000,
   },
-})
+}))
