5d0f0d79275f4653ed640f9b12caf34a36a99b7b
Justin Lin <wancat@wancat.cc>
2026-07-27 16:27:04 +1000

    feat: change default path to /nillumbik for now

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: loadEnv(mode, '.').VITE_BASE_PATH || '/forestportal',
+  base: loadEnv(mode, '.').VITE_BASE_PATH || '/nillumbik',
   server: {
     port: 3000,
   },
